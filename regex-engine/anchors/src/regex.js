fdebbe9ce609266707b88c04c2232282904a7e5d
Daniel Bazaco <danielbazacovelasco@gmail.com>
2022-04-11 11:18:20 +0200

    Anchors post.

---
+++ regex-engine/anchors/src/regex.js
@@ -0,0 +1,24 @@
+const parseRegex =  require("../../grammar/parser.js");
+const { NFABuilder } = require("./nfaBuilder.js");
+
+
+class NFARegex {
+    constructor (regex, modes) {
+        this.source = regex;
+        const ast = parseRegex(regex);
+        const builder = new NFABuilder(modes);
+        this.nfa = builder.regexToNFA(ast);
+    }
+
+    compute(string) {
+        const memory =  this.nfa.compute(string);
+        if (!memory) return null;
+        const result = {groups: {}};
+        for (const [group, start, end] of Object.values(memory.GROUP_MATCHES)) {
+            result.groups[group] = string.substring(start, end);
+        }
+        return result;
+    }
+}
+
+Object.assign(exports, {NFARegex});
